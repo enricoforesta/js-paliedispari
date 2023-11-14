668f8a5074e58b992612344c654297f3c4197190
enricoforesta <enrico.foresta97@gmail.com>
2023-11-14 20:00:25 +0100

    fix: esercizio 2

--- Palindroma/JS/main.js
+++ Palindroma/JS/main.js
@@ -5,41 +5,31 @@
 function parolaInvertita(parolaInput) {
     // Array della parola inserita
     const arrayInput = [];
-
     for (let i = 0; i < parolaInput.length; i++) {
         arrayInput.push(parolaInput[i])
     }
-
     console.log({ arrayInput });
 
-
+    // Array invertito della parola inserita
     const arrayInputInvertito = [];
     for (let i = parolaInput.length - 1; i >= 0; i--) {
         arrayInputInvertito.push(parolaInput[i])
     }
     console.log({ arrayInputInvertito })
 
-    // for (let i = 0; i < parolaInput.length; i++) {
-    //     if (arrayInput[i] === arrayInputInvertito[i]) {
-    //         console.log("è palindroma")
-    //     } else {
-    //         console.log("non è palindroma")
-    //     }
-    // }
-    let palindroma = false;
+    //Stampo se la parola è Palindroma o no
+    let palindroma = false
 
     for (let i = 0; i < parolaInput.length; i++) {
         if (arrayInput[i] === arrayInputInvertito[i]) {
-            palindroma = true;
-        }
+             palindroma = true
+        } 
     }
-    if(palindroma === true){
-        console.log("La parola è palindroma")
+    if (palindroma === true) {
+        console.log("La parola è palindroma");
+    } else {
+        console.log("La parola non è palindroma");
     }
-    else{
-        console.log("La parola non è palindroma")
-    }
-
 }
 
 
